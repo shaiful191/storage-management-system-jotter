ef3d1d556baeaf2dbeabe1a0c82878024857216c
Shaiful191 <saifulislamridoy1999@gmail.com>
2025-02-10 17:52:03 +0600

    file delete option done

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -49,3 +49,41 @@ export const getAllFilesController = async (req, res) => {
     res.status(500).json({ msg: error.message });
   }
 };
+
+  export const getRecentFilesController = async (req, res) => { 
+  try {
+    const recentFiles = await File.find({ userId: req.user._id })
+      .sort({ uploadDate: -1 }) 
+      .limit(3); 
+
+    if (recentFiles.length === 0) {
+      return res.status(404).json({ msg: 'No file found' });
+    }
+
+    res.status(200).json({ files: recentFiles });
+  } catch (error) {
+    console.error('Error fetching recent files:', error);
+    res.status(500).json({ msg: 'Error fetching recent files', error: error.message });
+  }
+};
+
+
+  export const deleteFileController = async (req, res) => {
+  try {
+    const fileId = req.params.id;
+
+    // Find the file by ID and user ID
+    const file = await File.findOne({ _id: fileId, userId: req.user._id });
+
+    if (!file) {
+      return res.status(404).json({ msg: 'File not found or unauthorized' });
+    }
+
+    await File.deleteOne({ _id: fileId }); // Delete file
+
+    res.status(200).json({ msg: 'File deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting file:', error);
+    res.status(500).json({ msg: 'Error deleting file', error: error.message });
+  }
+};
--- routes/fileRoutes.js
+++ routes/fileRoutes.js
@@ -1,20 +1,22 @@
 import express from 'express';
 import authMiddleware from '../middleware/authMiddleware.js';
-import { uploadFileController,getAllFilesController } from '../controllers/fileController.js';
+import { uploadFileController, getAllFilesController, getRecentFilesController, deleteFileController } from '../controllers/fileController.js';
 import multer from 'multer';
-import File from '../models/fileModel.js'; 
+import File from '../models/fileModel.js';
 
 const router = express.Router();
 
 
-const upload = multer({ 
+const upload = multer({
   storage: multer.memoryStorage(),
   limits: { fileSize: 10 * 1024 * 1024 } // 10 MB
 
-}); 
+});
 
 router.post('/upload', authMiddleware, upload.single('file'), uploadFileController);
 router.get('/all', authMiddleware, getAllFilesController);
+router.get('/recent', authMiddleware, getRecentFilesController);
+router.delete('/delete-one/:id', authMiddleware, deleteFileController);
 
 
 export default router;
