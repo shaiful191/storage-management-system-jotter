f3dfcc0819e2778fcfe6caec14be934d1de6109a
Shaiful191 <saifulislamridoy1999@gmail.com>
2025-02-11 22:34:36 +0600

    folder organizsed

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import passport from "passport";
 import "./config/passport.js";
-import fileRoutes from './routes/fileRoutes.js';
+import homeRoutes from './routes/homeRoutes.js';
 import favoriteRoutes  from './routes/favoriteRoutes.js';
 
 dotenv.config();
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(passport.initialize());
 
 app.use("/api/auth", authRoutes);
-app.use('/api/files', fileRoutes);
+app.use('/api/files', homeRoutes);
 app.use('/api/favorite', favoriteRoutes);
 
 export default app;
