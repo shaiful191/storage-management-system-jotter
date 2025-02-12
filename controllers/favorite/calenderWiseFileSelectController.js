a46b0bf38c32358f3bd75a6df414fe167201d767
Shaiful191 <saifulislamridoy1999@gmail.com>
2025-02-12 13:06:24 +0600

    getAllFilesByDate api done

--- controllers/favorite/calenderWiseFileSelectController.js
+++ controllers/favorite/calenderWiseFileSelectController.js
@@ -5,14 +5,14 @@ export const getAllFilesByDate = async (req, res) => {
   try {
     const { date } = req.params;
 
-    // Convert the date string to a Date object
+    
     const startOfDay = new Date(date);
     startOfDay.setHours(0, 0, 0, 0);
 
     const endOfDay = new Date(date);
     endOfDay.setHours(23, 59, 59, 999);
 
-    // Find files uploaded on the specified date
+    
     const files = await File.find({
       uploadDate: {
         $gte: startOfDay,
